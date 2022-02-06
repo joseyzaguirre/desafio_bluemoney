d6806fb2a1b0580b3924e1832d9b3e0d0f8b1527
José Eyzaguirre <josemanuel.eyzaguirre@gmail.com>
2022-02-06 00:29:27 -0300

    daplicación por el momento sólo convierte a dólar

--- index.js
+++ index.js
@@ -1,44 +1,34 @@
 const axios = require('axios');
-const fs = require('fs');
+const fs = require('fs').promises;
 
-/*
+
 if (process.argv.length < 6) {
     console.log('Se requeriere a lo menos 6 parámetros para ejecutar la aplicación');
     process.exit(1);
 };
-*/
+
 
 const nombre_archivo = process.argv[2] + '.' + process.argv[3];
 const divisa = process.argv[4];
 const pesos = process.argv[5];
 
-/*
-const conversion = parseInt(pesos) * parseInt(infodolar.valor);
-const mensaje = `
-    A la fecha: ${infodolar.fecha} 
-    Fue realizada cotización con los siguientes datos: 
-    Cantidad de pesos a convertir: ${pesos} 
-    Convertido a "dólar" da un total de: 
-    ${conversion};
-`
-*/
 
 //console.log(nombre_archivo)
 
-async function getData() {
+async function convertirDivisa() {
 
     let resp = await axios.get('https://mindicador.cl/api');
     let infodolar = resp.data.dolar
 
-    const conversion = parseInt(pesos) / parseInt(infodolar.valor);
+    const conversion = (parseInt(pesos)/parseInt(infodolar.valor)).toFixed(2);
     const mensaje = `
-    A la fecha: ${infodolar.fecha} 
-    Fue realizada cotización con los siguientes datos: 
-    Cantidad de pesos a convertir: ${pesos} 
-    Convertido a "${infodolar.nombre}" da un total de: 
-    ${conversion};
-`
-    console.log(mensaje)
+A la fecha: ${infodolar.fecha} 
+Fue realizada una cotización con los siguientes datos: 
+Cantidad de pesos a convertir: ${pesos} 
+Convertido a "${divisa}" da un total de: 
+${conversion};
+    `
+    await fs.writeFile (nombre_archivo, mensaje, 'UTF-8');
 }
 
-getData();
+convertirDivisa();
